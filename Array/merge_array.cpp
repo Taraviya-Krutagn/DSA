243aebf5af24f92a36805cb7fcea2185bfdd5253
Krutagn Taraviya <krutagn.study@gmail.com>
2026-07-30 19:29:35 +0530

    updated

---
+++ Array/merge_array.cpp
@@ -0,0 +1,66 @@
+// Write a program to merge two unsorted arrays.
+
+#include <iostream>
+using namespace std;
+
+// Function to input array elements
+void inputArray(int arr[], int size)
+{
+    cout << "Enter " << size << " elements: ";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+}
+
+// Function to merge two arrays
+void mergeArrays(int arr1[], int n1, int arr2[], int n2, int merged[])
+{
+    int k = 0;
+
+    for (int i = 0; i < n1; i++)
+    {
+        merged[k++] = arr1[i];
+    }
+
+    for (int i = 0; i < n2; i++)
+    {
+        merged[k++] = arr2[i];
+    }
+}
+
+// Function to display an array
+void displayArray(int arr[], int size)
+{
+    cout << "Merged Array: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n1, n2;
+
+    cout << "Enter size of first array: ";
+    cin >> n1;
+    int arr1[n1];
+
+    inputArray(arr1, n1);
+
+    cout << "Enter size of second array: ";
+    cin >> n2;
+    int arr2[n2];
+
+    inputArray(arr2, n2);
+
+    int merged[n1 + n2];
+
+    mergeArrays(arr1, n1, arr2, n2, merged);
+
+    displayArray(merged, n1 + n2);
+
+    return 0;
+}
