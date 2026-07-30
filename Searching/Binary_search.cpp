b42b61c7ebd61f3a7059b74ae2a08bc41a79cdeb
Krutagn Taraviya <krutagn.study@gmail.com>
2026-07-30 19:24:47 +0530

    something new added

---
+++ Searching/Binary_search.cpp
@@ -0,0 +1,69 @@
+// Write a programm to implement binary search.
+// its work only on Monotonic elements (Sorted array).
+
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int key)
+{
+    int start = 0;
+    int end = size - 1;
+    int mid = (start + end) / 2;
+
+    while(start <= end)
+    {
+        if(key == arr[mid])
+        {
+            return mid;
+            break;
+        }
+        else if(key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else if(key < arr[mid])
+        {
+            end = mid - 1;
+        }
+
+        mid = (start + end) / 2;
+    }
+
+    return -1;
+}
+
+int main()
+{
+    // Take size of array
+    int size;
+    cout << "Enter size of an array : ";
+    cin >> size;
+
+    int arr[size];
+
+    // Take array element
+    cout << "Enter array element(Monotonic array) : " << endl;
+
+    for(int i = 0; i < size; i++)
+    {
+        cout << "Enter " << i+1 << "th Element : ";
+        cin >> arr[i];
+    }
+
+    // take key value
+    int key;
+    cout << "Enter element which you want to find : ";
+    cin >> key;
+
+    int result = binarySearch(arr, size, key);
+
+    if(result == -1)
+    {
+        cout << "Element not found.";
+    }
+    else{
+        cout << "index of " << key << " is : " << result;
+    }
+
+    return 0;
+}
